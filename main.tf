86dd426ac0eac04aeadc294f1c1c0632f62d5b09
root <root@ip-172-31-13-12.ap-south-1.compute.internal>
2025-03-28 06:09:06 +0000

    firewall is now open after adding tags

--- main.tf
+++ main.tf
@@ -11,39 +11,41 @@ provider "google" {
   credentials = "credentials.json"
   project     = "eastern-adapter-455005-c6"
   region      = "asia-south1"
-  zone        = "asia-south1-a"
+  zone        = "asia-south1-b"
 }
 
 resource "google_compute_instance_template" "management_template" {
   name         = "management-template"
   machine_type = "e2-micro"
-
+  tags = ["ssh", "http-server", "https-server"]
+ 
   disk {
     auto_delete  = true
     boot         = true
-    source_image = "projects/ubuntu-os-cloud/global/images/family/ubuntu-2004-lts"
+    source_image = "ubuntu-os-cloud/ubuntu-2004-lts"
   }
 
   network_interface {
     network = "default"
     access_config {}
   }
+  
 }
 
 
-resource "google_compute_firewall" "wordpress_ingress" {
-  name    = "allow-ssh-http-https"
+resource "google_compute_firewall" "ingress" {
+  name    = "my-ports"
   network = "default"
   allow {
     protocol = "tcp"
     ports    = ["22", "80", "443"]
   }
   source_ranges = ["0.0.0.0/0"]
+  target_tags = ["ssh", "http-server", "https-server"]
 }
-
 resource "google_compute_instance_from_template" "vm_from_template" {
   name     = "management"
-  zone     = "asia-south1-a"
+  zone     = "asia-south1-b"
   source_instance_template = google_compute_instance_template.management_template.id
 }
 
