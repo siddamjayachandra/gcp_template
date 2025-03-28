609dc525f0c18459f54ed57bf502edbc0428ef4f
root <root@ip-172-31-13-12.ap-south-1.compute.internal>
2025-03-28 06:46:32 +0000

    removed https from the tags

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ provider "google" {
 resource "google_compute_instance_template" "management_template" {
   name         = "management-template"
   machine_type = "e2-micro"
-  tags = ["ssh", "http-server", "https-server"]
+  tags = ["ssh", "http-server"]
  
   disk {
     auto_delete  = true
@@ -41,7 +41,7 @@ resource "google_compute_firewall" "ingress" {
     ports    = ["22", "80", "443"]
   }
   source_ranges = ["0.0.0.0/0"]
-  target_tags = ["ssh", "http-server", "https-server"]
+  target_tags = ["ssh", "http-server"]
 }
 resource "google_compute_instance_from_template" "vm_from_template" {
   name     = "management"
