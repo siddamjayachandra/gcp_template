79e193432cdbfddd2e5b13e7658c22b239583e28
root <root@ip-172-31-13-12.ap-south-1.compute.internal>
2025-03-28 08:31:07 +0000

    without port numbers also able to see the firewall in on status

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ provider "google" {
 resource "google_compute_instance_template" "management_template" {
   name         = "management-template"
   machine_type = "e2-micro"
-  tags = ["ssh", "http-server"]
+  tags = ["ssh", "http-server", "https-server"]
  
   disk {
     auto_delete  = true
@@ -38,10 +38,9 @@ resource "google_compute_firewall" "ingress" {
   network = "default"
   allow {
     protocol = "tcp"
-    ports    = ["22", "80", "443"]
   }
   source_ranges = ["0.0.0.0/0"]
-  target_tags = ["ssh", "http-server"]
+  target_tags = ["ssh", "http-server", "https-server"]
 }
 resource "google_compute_instance_from_template" "vm_from_template" {
   name     = "management"
